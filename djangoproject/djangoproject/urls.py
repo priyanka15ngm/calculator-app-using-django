330c6df3a77dfc198533c946a72208b5d398df31
priyanka nigam <priyanka.nigam_cs18@gla.ac.in>
2021-01-25 16:49:07 +0530

    url change

--- djangoproject/djangoproject/urls.py
+++ djangoproject/djangoproject/urls.py
@@ -18,7 +18,7 @@ from django.urls import path
 from django.conf.urls import include
 
 urlpatterns = [
-    path('', include('rootapp.urls')),
+    #path('', include('rootapp.urls')),
     path('admin/', admin.site.urls),
-    path('calculatorapp', include('calculatorapp.urls'))
+    path('', include('calculatorapp.urls'))
 ]
